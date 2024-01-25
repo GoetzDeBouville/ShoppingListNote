2ad2c823d12d16b69241bfe1a362a4ef1cd4d74a
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-01-25 13:10:28 +0300

    Add event class

--- app/src/main/java/com/goetzgegouville/myapplication/presentation/shoppinglistscreen/ShoppingListEvent.kt
+++ app/src/main/java/com/goetzgegouville/myapplication/presentation/shoppinglistscreen/ShoppingListEvent.kt
@@ -1,7 +1,10 @@
 package com.goetzgegouville.myapplication.presentation.shoppinglistscreen
 
-import com.goetzgegouville.myapplication.data.models.ShoppingListItemEntity
+import com.goetzgegouville.myapplication.domain.models.ShoppingListItem
 
 sealed class ShoppingListEvent {
-    data class OnShowDialog(val item: ShoppingListItemEntity)
+    data class OnShowDeleteDialog(val item: ShoppingListItem) : ShoppingListEvent()
+    data class OnShowEditDialog(val item: ShoppingListItem) : ShoppingListEvent()
+    data class OnItemClick(val item: ShoppingListItem) : ShoppingListEvent()
+    data object OnSave : ShoppingListEvent()
 }
